b4cc557c2242995d7583fd4fe7eaa9a805303336
opuopu <nurmdopu428@gmail.com>
2024-02-25 12:12:26 +0600

    refactor:change ip

--- src/utils/Image.jsx
+++ src/utils/Image.jsx
@@ -1,5 +1,5 @@
 const ImageGenerator = (url) => {
-  return url ? `${`http://143.198.26.67:3000`}${url}` : null;
+  return url ? `${`http://64.23.212.196:3000`}${url}` : null;
 };
 
 export default ImageGenerator;
