4d3d008433818f8df0fd619f13354ee135c89ec3
opuopu <nurmdopu428@gmail.com>
2024-02-27 16:53:35 +0600

    feat: added new features

--- src/Redux/api/baseApi.js
+++ src/Redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { tagTypesList } from "../../types/tags";
 import { logout } from "../features/auth/authSlice.js";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://192.168.10.3:3000/api",
+  baseUrl: `${process.env.REACT_APP_BACKEND_URL}/api`,
   // credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
